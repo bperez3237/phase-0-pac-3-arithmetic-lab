fdf9608812b17f4a852ad2c9e743b235f9d42894
Brian <b.perez3237@gmail.com>
2022-01-19 19:38:08 -0500

    CoMpLeTeD Assignment

---
+++ index.js
@@ -0,0 +1,33 @@
+function add(n,m) {
+    return n+m;
+}
+
+function subtract(n,m) {
+    return n-m;
+}
+
+
+function multiply(n,m) {
+    return n*m;
+}
+
+
+function divide(n,m) {
+    return n/m;
+}
+
+function increment(n) {
+    return n +=1
+}
+
+function decrement(n) {
+    return n -=1
+}
+
+function makeInt(string) {
+    return parseInt(string,10);
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
